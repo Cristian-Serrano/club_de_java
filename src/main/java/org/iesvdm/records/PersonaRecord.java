cecda916dd4987eff83569796388cea0115f2887
toteabe <toteabe@yahoo.es>
2023-10-22 21:54:34 +0200

    21 DOP - DATA ORIENTED PROGRAMMING

--- src/main/java/org/iesvdm/records/PersonaRecord.java
+++ src/main/java/org/iesvdm/records/PersonaRecord.java
@@ -2,6 +2,10 @@ package org.iesvdm.records;
 
 import java.time.LocalDate;
 
+//USA records EN EL PARADIGMA:
+//DOP - DATA ORIENTED PROGRAMMING - PROGRAMACIÓN ORIENTADA A DATOS
+//FRENTE class EN EL PARADIGMA
+//OOP - OBJECT ORIENTED PROGRAMMING - PROGRAMACIÓN ORIENTADA A OBJETOS
 public record PersonaRecord(String nombre,
                             String apellidos,
                             LocalDate fechaNacimiento,
